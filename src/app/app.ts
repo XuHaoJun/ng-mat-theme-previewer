7694bf175d10cb472252508f778e857086fbc4a3
xuhaojuntw <xuhaojuntw@gmail.com>
2025-06-28 20:35:07 +0800

    import material example first try

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+ import { ButtonOverviewExample } from './@angular/components/src/components-examples/material/button/button-overview/button-overview-example';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSlideToggleModule],
+  imports: [RouterOutlet, MatSlideToggleModule, MatButtonModule,
+ButtonOverviewExample
+     ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
